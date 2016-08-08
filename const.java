de2c67731494fb21bc2ff8576eb0e9242abfa2d7
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-08-08 20:33:02 +0530

    Add files via upload

---
+++ const.java
@@ -0,0 +1,30 @@
+import java.io.*;
+class sample
+{
+  private int id;
+  sample()
+  {
+    
+    id=101;
+    System.out.println("default constructor"+id);
+    }
+    sample(int no)
+    {
+      id=no;
+      System.out.println("one argument"+id);
+      }
+      }
+      public class const Demo;
+      {
+        publicstatic void main(string[] args)
+        {
+          
+          sample s1=new sample();
+          sample s2=new sample(102);
+          }
+          }
+
+
+    
+
+    
